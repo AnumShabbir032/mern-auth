f260004916371e3bb1c67d1e29979379aa95da70
AnumShabbir032 <AnumShabbir032@gmail.com>
2024-03-07 01:36:04 +0500

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to MongoDb');
+})
+.catch(() =>{
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(3000, ()=> {
     console.log('Sever listening on port 3000');
 })
+
 
 
 
